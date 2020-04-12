69ca8c531015382db7eedfa9c3d93ba5d167029b
dpefley <dpefley@wustl.edu>
2020-04-11 19:16:51 -0700

    Updating JS - WIP

---
+++ js/stat_maps.js
@@ -0,0 +1,34 @@
+const batting_stats_map = {
+    Season: "season",
+    AB: "ab",
+    H: "h",
+    '2B': "d",
+    '3B': "t",
+    R: "r",
+    HR: "hr",
+    RBI: "rbi",
+    SB: "sb",
+    BB: "bb",
+    K: "so",
+    AVG: "avg",
+    OBP: "obp",
+    SLG: "slg",
+    OPS: "ops",
+    BABIP: "babip"
+}
+
+const pitching_stats_map = {
+    Season: "season",
+    IP: "ip",
+    G: "g",
+    GS: "gs",
+    ERA: "era",
+    WHIP: "whip",
+    K: "so",
+    BB: "bb",
+    'HR/9': "hr9",
+    AVG: "avg",
+    OBP: "obp",
+    SLG: "slg",
+    OPS: "ops"
+}
